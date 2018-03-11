3c8272689b545818739f8c85fcba3a308b50fa4f
ARTHAM <sa458e@att.com>
2018-03-10 19:13:09 -0800

    first git branch created with node server project

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error) => {
+    console.log('not able to append the server logs');
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintaince.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) =>{
+  //res.send('<h1>Hello Express</h1>');
+  res.render('home.hbs',{
+    pageTitle: 'This is my Home Page',
+    pageHeader: 'This is my node express practice Page',
+    pageContent: 'Welcome to My first Express hbs Example on Home Page'
+  });
+  // res.send({
+  //   name: 'Sreenivas',
+  //   state: 'Telangana',
+  //   like: ['politics', ' business', 'career', 'life']
+  // });
+});
+
+app.get('/about', (req,res) =>{
+  //res.send('About Page !');
+  res.render('about.hbs',{
+    pageTitle: 'This is my About Page',
+    pageHeader: 'This is my node express practice Page',
+    pageContent: 'Welcome to My first Express hbs Example'
+  });
+});
+
+app.get('/bad', (req,res) =>{
+  res.send({
+    errorMessage: 'Not able to llaod the page',
+    errorCode: 404,
+    errorDescription: 'Not able to find the requestesd resource'
+});
+});
+
+app.listen(3000, () => {
+  console.log('Server is up and running on the port numner 3000');
+});
